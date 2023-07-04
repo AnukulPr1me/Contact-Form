967c7556a31c505c7ea21236afdca9b424805788
Anukul <101039186+AnukulPr1me@users.noreply.github.com>
2023-07-04 13:05:47 +0530

    final update
    has an error I thinks its from  SMPTJS side which provide email service

---
+++ src/script.js
@@ -0,0 +1,33 @@
+const fullName = document.getElementById('name');
+const email = document.getElementById('email');
+const phoneNumber = document.getElementById('phone');
+const Country = document.getElementById('country');
+const message = document.getElementById('message');
+const submit = document.getElementsByClassName('form-submit')[0];
+
+
+
+submit.addEventListener('submit', (e)=>{
+
+    e.preventDefault();
+    console.log("clicked");
+    let ebody = `
+    <b>Name: </b>${fullName.value}
+    <b>Email: </b>${email.value}
+    <b>Phone: </b>${phoneNumber.value}
+    <b>Message: </b>${message.value}
+    <b>Country: </b>${Country.value}
+
+    `
+    Email.send({
+        SecureToken : "C973D7AD-F097-4B95-91F4-40ABC5567812",
+        To : 'n0one4u2kill@gmail.com',
+        From : "n0one4u2kill@gmail.com",
+        Subject : "This is the subject" + email.value,
+        Body : ebody
+    }).then(
+        message => alert("Message sent successfully")
+    );
+
+});
+
